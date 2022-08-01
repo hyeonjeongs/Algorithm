7bb141c6ec4a6db597aa879338b3646fc1e33468
hyeonjeong Park <py0429@ewhain.net>
2022-08-01 18:18:24 +0900

    greedy

--- Greedy/11047/11047.cpp
+++ Greedy/11047/11047.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 vector<int> coin;
+void coinNum(int n, int k) {
+	int cnt = 0; //동전 개수
+	for (int i = n - 1; i >= 0; i--) {
+		if (k == 0)
+			break;
+		cnt += k / coin[i];
+		k = k % coin[i]; //나머지 돈
+	}
+	cout << cnt;
+}
 int main() {
 	int n, k;
 	cin >> n >> k;
@@ -13,5 +23,6 @@ int main() {
 		cin >> coin[i];
 	}
 
+	coinNum(n, k);
 	return 0;
 }
