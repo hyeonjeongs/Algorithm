a612a34b87e5ed649c9228c856c9669403bc76c2
hyeonjeong Park <py0429@ewhain.net>
2022-08-03 01:04:02 +0900

    sort

--- Sort/10814/10814.cpp
+++ Sort/10814/10814.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
+typedef pair<int, string> ci;
 
+vector<ci> members;
+bool cmp(const ci& a, const ci& b) {
+	return a.first < b.first;
+}
 int main() {
+	int n;
+	cin >> n;
 
+	members.assign(n,ci(0," "));
+	for (int i = 0; i < n; i++) {
+		cin >> members[i].first >> members[i].second;
+	}
+
+	stable_sort(members.begin(), members.end(), cmp);
+
+	for (int i = 0; i < n; i++) 
+		cout << members[i].first << " " << members[i].second << '\n';
+	
+	return 0;
 }
