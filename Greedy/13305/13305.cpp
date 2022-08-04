1fadfb00cfa8cdb566609fee6279dec2047d9101
hyeonjeong Park <py0429@ewhain.net>
2022-08-04 10:59:57 +0900

    greedy

---
+++ Greedy/13305/13305.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+typedef long long ll;
+vector<ll> road;
+vector<ll> city;
+ll minCost(int n) {
+	ll totCost = 0;
+	ll curCost = city[0];
+
+	for (int i = 0; i < n-1; i++) {
+		if (curCost > city[i]) // 기름값이 더 저렴한면 교체
+			curCost = city[i];
+		totCost += curCost * road[i];
+	}
+	return totCost;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	road.assign(n - 1, 0);
+	city.assign(n, 0);
+
+	for (int i = 0; i < n - 1; i++)
+		cin >> road[i];
+	for (int i = 0; i < n; i++)
+		cin >> city[i];
+	
+	cout << minCost(n);
+
+	return 0;
+}
