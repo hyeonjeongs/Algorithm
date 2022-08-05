4ce85fbd00b0d43e77dfa2119da0b50e532884c4
hyeonjeong Park <py0429@ewhain.net>
2022-08-05 16:11:01 +0900

    dp

--- Dynamic Programming/11053/11053.cpp
+++ Dynamic Programming/11053/11053.cpp
@@ -7,9 +7,8 @@ vector<int> num;
 vector<int> dp;
 int maxLength(int n) {
 	int maxN = 0;
-	
-	dp[0] = 1;
-	for (int i = 1; i < n; i++) {
+
+	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < i; j++) {
 			if (num[i] > num[j]) 
 				dp[i] = max(dp[i], dp[j] + 1);
