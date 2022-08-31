23be8e23df231f8c90c057b6d001db89cfb61028
hyeonjeong Park <py0429@ewhain.net>
2022-09-01 01:13:57 +0900

    priority queue

---
+++ Priority queue/11279.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+
+void solve(int input, priority_queue<int, vector<int>>& pq) {
+    if (input == 0) {
+        if (pq.empty())
+            cout << 0 << '\n';
+        else {
+            cout << pq.top() << '\n';
+            pq.pop();
+        }
+    }
+    else if (input > 0) {
+        pq.push(input);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int n;
+    priority_queue<int, vector<int>> pq;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int input;
+        cin >> input;
+        solve(input, pq);
+    }
+
+    return 0;
+
+}
