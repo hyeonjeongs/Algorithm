d6c32ae1f1252eaab9e6bb928a53714f8c6f2b7d
hyeonjeong Park <py0429@ewhain.net>
2022-09-07 21:57:31 +0900

    map, set

---
+++ map, set/2002.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<map>
+#include <vector>
+
+using namespace std;
+
+// 추월한 차가 몇 개인지 구하는 함수
+int outCar(int n, vector<string> &out_car, map<string, int> &cars){
+    int ans = 0; //몇 개의 차가 추월했는지
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(cars[out_car[i]]>cars[out_car[j]]){
+                ans++;
+                break;
+            }
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<string> out_car(n, "");
+
+    map<string, int> cars;
+    string input;
+    for (int i = 1; i <= n; i++) {
+        cin >> input;
+        cars[input] = i;
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> out_car[i];
+    }
+    cout<< outCar(n, out_car, cars);
+
+    return 0;
+}
