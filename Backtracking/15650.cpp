e2221560f197188100df00fc1a0a2daa66de5e37
hyeonjeong Park <py0429@ewhain.net>
2022-09-23 14:07:08 +0900

    backtracking

--- Backtracking/15650.cpp
+++ Backtracking/15650.cpp
@@ -1,3 +1,28 @@
-//
-// Created by hyeon on 2022-09-23.
-//
+#include<iostream>
+#include<vector>
+
+using namespace std;
+const int SIZE = 8;
+
+int n, m;
+vector<int> num(SIZE);
+
+void backTracking(int index, int start) {
+    if (index == m) {
+        for (int i = 0; i < m; i++)
+            cout << num[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for (int i = start; i <= n; i++) {
+        num[index] = i;
+        backTracking(index + 1, i + 1); //다음 인덱스 호출
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    backTracking(0, 1);
+
+    return 0;
+}
