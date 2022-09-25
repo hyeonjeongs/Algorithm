8a47206ccca63951d431547fe3253d7b2c1c05f4
hyeonjeong Park <py0429@ewhain.net>
2022-09-25 12:34:14 +0900

    backtracking

--- Backtracking/9663.cpp
+++ Backtracking/9663.cpp
@@ -9,22 +9,21 @@ bool check_col[SIZE]; //열 체크
 bool check_left[SIZE * 2]; //좌하향 대각선 체크
 bool check_right[SIZE * 2]; //우하향 대각선 체크
 
-void backtracking(int cnt) { //cnt: 행
-    if (cnt == n) { //기저조건, n개의 퀸이 모두 놓임
-        ans++; //정답 증가
+void backtracking(int row) { //row: 행
+    if (row == n) {
+        ans++;
         return;
     }
-    for (int i = 0; i < n; i++) { //i: 열
-        if (!check_col[i] && !check_left[cnt + i] && !check_right[cnt - i + n]) { //모두 false여야 배치 가능
-            //(cnt, i)에 퀸 배치 -> 그에 따른 열, 좌하향 대각선, 우하향 대각선 체크해줌
+    for (int i = 0; i < n; i++) {
+        if (!check_col[i] && !check_left[row + i] && !check_right[row - i + n]) {
             check_col[i] = true;
-            check_left[cnt + i] = true;
-            check_right[cnt - i + n] = true;
-            backtracking(cnt + 1); //다음 행 호출
-            //재탐색이 가능하도록 원래상태로 돌려놓음
+            check_left[row + i] = true;
+            check_right[row - i + n] = true;
+            backtracking(row + 1);
+            //원래 상태로 돌려줘야함
             check_col[i] = false;
-            check_left[cnt + i] = false;
-            check_right[cnt - i + n] = false;
+            check_left[row + i] = false;
+            check_right[row - i + n] = false;
         }
     }
 
