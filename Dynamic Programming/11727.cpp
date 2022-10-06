28e35f9b259c383d0d8fe974b93767404dd0b701
hyeonjeong Park <py0429@ewhain.net>
2022-10-07 01:05:17 +0900

    dp

--- Dynamic Programming/11727.cpp
+++ Dynamic Programming/11727.cpp
@@ -5,21 +5,21 @@ using namespace std;
 int dp[1000];
 
 int tileDp(int n) {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
     dp[0] = 1;
     dp[1] = 3;
-    for (int i = 2; i < n; i++) { //��ȭ�� �����
+    for (int i = 2; i < n; i++) {
         dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
     }
     return dp[n - 1];
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     int n;
 
+    // 입력
     cin >> n;
     cout << tileDp(n);
 
