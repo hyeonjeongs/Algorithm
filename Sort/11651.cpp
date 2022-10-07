d201bd98055e81dbb2683fc7911acfc36f9b9a7e
hyeonjeong Park <py0429@ewhain.net>
2022-10-08 01:52:04 +0900

    sort'

--- Sort/11651.cpp
+++ Sort/11651.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 vector<pair<int, int>> arr;
 
+// 오름차순 정렬
 bool cmp(pair<int, int> a, pair<int, int> b) {
     if (a.second == b.second)
         return a.first < b.first;
@@ -16,6 +17,8 @@ bool cmp(pair<int, int> a, pair<int, int> b) {
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
+
+    // 입력
     int num;
     cin >> num;
 
@@ -25,9 +28,11 @@ int main() {
         arr.push_back(pair<int,int>(x,y));
     }
 
+    //사용자 정의 함수 정렬
     sort(arr.begin(), arr.end(),cmp);
 
 
+    // 출력
     for (int i = 0; i < num; i++) {
         cout << arr[i].first << ' ' << arr[i].second<<'\n';
     }
