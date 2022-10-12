83a403edfef0b4da972d94cfd454c1a04888edd2
hyeonjeong Park <py0429@ewhain.net>
2022-10-12 23:01:36 +0900

    priority queue:

--- Priority queue/11279.cpp
+++ Priority queue/11279.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 void solve(int input, priority_queue<int, vector<int>>& pq) {
     if (input == 0) {
-        if (pq.empty())
+        if (pq.empty()) //빈경우 처리
             cout << 0 << '\n';
         else {
             cout << pq.top() << '\n';
@@ -23,8 +23,9 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     int n;
-    priority_queue<int, vector<int>> pq;
+    priority_queue<int, vector<int>> pq; //우선순위 큐 사용하기
 
+    //입력
     cin >> n;
     for (int i = 0; i < n; i++) {
         int input;
