1f475d90d37d1c5a777c8ed03877669e8c2b12fd
hyeonjeong Park <py0429@ewhain.net>
2023-02-10 22:09:50 +0900

    backtracking

---
+++ Backtracking/15651.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int n, m;
+vector<int> nums;
+vector<bool> check;
+void backTracking(int index){
+
+    // 가지치기
+    if(index == m){
+        for(int i=0; i<nums.size(); i++){
+            cout<< nums[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=1; i<=n; i++){
+        nums[index] = i;
+        backTracking(index+1);
+    }
+
+}
+
+int main() {
+
+    cin >> n >> m;
+
+    nums.assign(m,0);
+    check.assign(n+1, false);
+
+    backTracking(0);
+
+    return 0;
+}
--- DFS, BFS/11724.cpp
+++ DFS, BFS/11724.cpp
@@ -13,7 +13,7 @@ void dfs(int cur) {
         if (!visited[next])
             dfs(next);
     }
-    
+
 }
 
 
