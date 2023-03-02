832d08219ef12eceadf7d8241b0b2c71a5c07860
hyeonjeong Park <py0429@ewhain.net>
2023-03-02 16:25:09 +0900

    dp

--- Dynamic Programming/2293.cpp
+++ Dynamic Programming/2293.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int makeCoin(vector<int> coin, int n, int k){
     vector<int> dp(k+1, 0);
 
-    dp[0] = 1;
+    dp[0] = 1; // 아무 동전 선택 안한 경우의 수 -> 1가지
     for(int i=0; i<n; i++){
         for(int j=coin[i]; j<=k; j++){
             dp[j] += dp[j-coin[i]]; // 2원을 이용하고 10원을 만드는 경우 [8]원에 저장된 데이터를 저장함
