9cc962e6f35764a7304a6c7a207f7934be458e29
hyeonjeong Park <py0429@ewhain.net>
2023-03-05 22:06:20 +0900

    swm 1번

--- Backtracking/15650.cpp
+++ Backtracking/15650.cpp
@@ -1,3 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 조합 이용
+int main(){
+    int n, m;
+    vector<bool> arr;
+
+    cin >> n >> m;
+    arr.assign(n+1,0);
+
+    for(int i=1; i<=m; i++){
+        arr[i] = true;
+    }
+
+    do{
+        for(int i=1; i<=n; i++){
+            if(arr[i]){
+                cout<< i <<' ';
+            }
+        }
+        cout<<'\n';
+
+    }while(prev_permutation(arr.begin()+1, arr.end()));
+
+    return 0;
+}
+
+/*
+
+// 백트래킹 이용
 #include<iostream>
 #include<vector>
 
@@ -25,4 +59,4 @@ int main() {
     backTracking(0, 1);
 
     return 0;
-}
+}*/
