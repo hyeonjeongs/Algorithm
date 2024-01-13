aba1d13d64c09edc6485a68574e82b8d0c59e3f4
hyeonjeongs <py0429@ewhain.net>
2024-01-13 17:41:52 +0900

    simulation

--- Simulation/1713.cpp
+++ Simulation/1713.cpp
@@ -1,57 +1,67 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 using namespace std;
-typedef pair<int,int> pi;
-map<int,int> recommendNum;
+typedef pair<int, int> pi;
+map<int, int> recommendNum;
 
-int recommendPresident(int n, int input, vector<pi> &recommend, int num) {
-    vector<int> order(2,0); // 오래된거
-    int cnt=100000;
+bool cpm(pi a, pi b) {
+    return a.first<b.first;
+}
 
-    if(recommend.size()<n) {
-        recommend.push_back(pi(input, recommend.size()+1));
-        recommendNum[input] = 1;
-        num =1;
-        return num;
+void recommendPresident(int n, int input, vector<pi> &recommend, int current_index) {
+    int cnt = 987654321, order=987654321, index=-1;
+
+    if (recommendNum.find(input) != recommendNum.end()) {
+        recommendNum[input]++;
+        return;
     }
 
-    // 추천수 가장 적은거 판단
-    for(int i=0; i<n; i++) {
-        if(recommend[i].second<cnt) {
-            cnt = recommend[i].second;
+    if (recommend.size() < n) {
+        recommend.push_back(pi(input, current_index)); // 학생숫자, 순서
+        recommendNum[input] = 1; // 추천받은 횟수
+        return;
+    }
+
+    // 빈자리 없고 추천수 가장 적은거 판단
+    for (int i = 0; i < n; i++) {
+        if(recommendNum[recommend[i].first] < cnt) {
+            cnt = recommendNum[recommend[i].first];
         }
     }
 
     // 추천수 가장 적은것들중 가장 오래된것 판단
-    for(int i=0; i<n; i++) {
-        if(recommend[i].second == cnt && order[1] <recommendNum[recommend[i].first]) { // 추천수 같은경우 & 오래된경우
-            order[0] = i;
-            order[1] = recommendNum[recommend[i].first];
+    for (int i = 0; i < n; i++) {
+        if (recommendNum[recommend[i].first] == cnt && recommend[i].second<order) { // 추천수 같은경우 & 오래된경우
+            order = recommend[i].second;
+            index = i;
         }
-
     }
 
+    recommendNum.erase(recommend[index].first);
+    recommend[index].first = input;
+    recommend[index].second = current_index;
+    recommendNum[input]=1;
 }
 
 int main() {
-    int n,m;
+    int n, m;
 
-    cin>>n>>m;
+    cin >> n >> m;
 
-    vector<pi> recommend(n);
+    vector<pi> recommend;
 
-    int num=0; //추천수
-
-    for(int i=0; i<m; i++) {
+    for (int i = 0; i < m; i++) {
         int input;
-        cin>>input;
-        num = recommendPresident(n,input, recommend, num);
+        cin >> input;
+        recommendPresident(n, input, recommend,i);
     }
 
-    for(int i=0; i<n; i++) {
-        cout<<recommend[i].first<<' ';
+    sort(recommend.begin(), recommend.end(), cpm);
+    for (int i = 0; i < recommend.size(); i++) {
+        cout << recommend[i].first << ' ';
     }
 
     return 0;
