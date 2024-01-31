1443e481877f532800a7a3c3ee605470a50b7629
hyeonjeongs <py0429@ewhain.net>
2024-02-01 00:11:29 +0900

    bruteforce

---
+++ Bruteforce/prog_소수찾기.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <math.h>
+using namespace std;
+
+bool isPrime(int num) {
+    if(num<2) {
+        return false;
+    }
+
+    for(int i=2; i<=sqrt(num);i++) {
+        if(num%i ==0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int solution(string numbers) {
+    int answer = 0;
+    set<int> result;
+
+    sort(numbers.begin(), numbers.end());
+
+    do{
+        string ans;
+        for(int i=0; i<numbers.size(); i++) {
+            ans+=numbers[i];
+            if(isPrime(stoi(ans))) {
+                result.insert(stoi(ans));
+            }
+        }
+
+    }while(next_permutation(numbers.begin(), numbers.end()));
+    answer = result.size();
+
+    return answer;
+}
