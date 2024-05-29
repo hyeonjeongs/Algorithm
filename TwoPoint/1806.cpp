d41fecafefba30d5a0165cf1467974c7a53302a0
hyeonjeongs <py0429@ewhain.net>
2024-05-30 00:31:13 +0900

    two point

--- TwoPoint/1806.cpp
+++ TwoPoint/1806.cpp
@@ -4,6 +4,43 @@
 using namespace std;
 
 int main() {
+    int left = 0, right = 0;
+
+    int n, s;
+    cin>>n>>s;
+    int cnt = n+1;
+
+    vector<int> num(n+1, 0);
+    for(int i=0; i<n; i++) {
+       cin >> num[i];
+    }
+    int sum = num[left];
+    while (left<=right && right <n) {
+
+        if(sum >= s) {
+            cnt = min(cnt, right-left+1);
+            sum -= num[left++];
+        }else {
+            sum += num[++right];
+        }
+
+    }
+    if(cnt == n+1) {
+        cnt =0;
+    }
+
+    cout << cnt <<'\n';
+    return 0;
+
+}
+
+/*
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
 
     int left = 1, right = 1;
 
@@ -36,4 +73,4 @@ int main() {
 
     cout << cnt <<'\n';
     return 0;
-}
+}*/
