3ba38c8911bbbccbd64083ceb4060115817295ff
hyeonjeongs <py0429@ewhain.net>
2024-09-04 07:04:45 +0900

    dfs

--- DFS, BFS/2606-2.cpp
+++ DFS, BFS/2606-2.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
 #include <vector>
+
+using namespace std;
+vector<vector<int>> virus;
+vector<bool> visited;
+int result=0;
+void dfs(int node) {
+
+    visited[node]= true;
+    result++;
+
+    for(int i=0; i<virus[node].size(); i++) {
+        int num = virus[node][i];
+        if(visited[num]) {
+            continue;
+        }
+        dfs(num);
+    }
+}
+
+int main() {
+
+    int n, m;
+    cin>> n >> m;
+    virus.assign(n+1, vector<int>());
+    visited.assign(n+1, false);
+
+    while(m--) {
+        int a, b;
+        cin >> a >> b;
+        virus[a].push_back(b);
+        virus[b].push_back(a);
+    }
+
+    dfs(1);
+    cout << result-1 <<'\n';
+    return 0;
+}
+
+
+/*
+#include <iostream>
+#include <vector>
 #include <stack>
 using namespace std;
 
@@ -58,4 +100,4 @@ int main() {
     cout<<depth-1;
     return 0;
 
-}
+}*/
