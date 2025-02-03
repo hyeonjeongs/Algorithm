75599024e1a9667efa89e8136e82696db0d1ff37
hyeonjeongs <py0429@ewhain.net>
2025-02-03 23:45:42 +0900

    dp

--- Dynamic Programming/10844.cpp
+++ Dynamic Programming/10844.cpp
@@ -1,5 +1,44 @@
 #include <iostream>
 #include <vector>
+
+using namespace std;
+const int MOD =  1e9;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> dp(n+1, vector<int>(10,0));
+    for(int i=1 ;i<10; i++) {
+        dp[1][i] = 1;
+    }
+    for(int i=2; i<=n; i++) {
+        for(int j=0; j<10; j++) {
+            if(j==0) {
+                dp[i][j]=dp[i-1][j+1] % MOD;
+                continue;
+            }
+            if(j==9) {
+                dp[i][j] = dp[i-1][j-1]%MOD;
+                continue;
+            }
+            dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1])%MOD;
+        }
+    }
+
+    int result = 0;
+    for(int i=0; i<10; i++) {
+        result = (result + dp[n][i]) % MOD;
+    }
+    cout << result <<'\n';
+    return 0;
+
+}
+
+
+
+/*
+#include <iostream>
+#include <vector>
 const int MOD = 1e9;
 using namespace std;
 
@@ -36,4 +75,4 @@ int main() {
     cout<< result <<'\n';
 
     return 0;
-}
+}*/
