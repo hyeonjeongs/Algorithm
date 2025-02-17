b7d8cf300753bd40e3c9123f030d5f1c12051756
hyeonjeongs <py0429@ewhain.net>
2025-02-17 23:54:17 +0900

    backtraking

--- Bruteforce/14500-2.cpp
+++ Bruteforce/14500-2.cpp
@@ -1,60 +1,64 @@
-#include <iostream>
+#include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
+
 
 using namespace std;
-
-// 11:20
 vector<vector<int>> arr;
 vector<vector<bool>> visited;
-int result = 0;
-int n, m;
+int maxs = 0;
 int dx[] = {0,0,1,-1};
-int dy[] = {1,-1,0,0};
-void dfs(int y, int x, int cnt, int sum){
+int dy[] = {-1,1,0,0};
+int n, m;
+using namespace std;
 
-    if(cnt>=4) {
-        result = max(sum, result);
+void backtrcking(int y, int x, int count, int sum) {
+    if(count>=4) {
+        maxs = max(maxs, sum);
         return;
     }
 
     for(int i=0; i<4; i++) {
-        int nx= x + dx[i];
-        int ny= y + dy[i];
-        if(nx>0 && nx<=m && ny >0 && ny<=n && !visited[ny][nx]) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(nx<0 || nx >=m || ny<0 || ny>=n) {
+            continue;
+        }
+        if(!visited[ny][nx]) {
             visited[ny][nx] = true;
-            if(cnt==2) {
-                dfs(y, x, cnt+1, sum+arr[ny][nx]);
+            backtrcking(ny, nx, count+1, sum+arr[ny][nx]);
+            if(count ==2) {
+                backtrcking(y, x, count+1, sum+arr[ny][nx]);
             }
-            dfs(ny, nx, cnt+1, sum+arr[ny][nx]);
-
             visited[ny][nx] = false;
         }
     }
+
+
 }
 
 int main() {
 
-    cin>> n>>m;
-    arr.assign(n+2, vector<int>(m+2,0));
-    visited.assign(n+2, vector<bool>(m+2,false));
-
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=m; j++) {
+    cin >> n >> m;
+    arr.assign(n, vector<int>(m,0));
+    visited.assign(n, vector<bool>(m,0));
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
             cin >> arr[i][j];
         }
     }
 
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=m; j++) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
             if(!visited[i][j]) {
                 visited[i][j] = true;
-                dfs(i, j, 1, arr[i][j]);
+                backtrcking(i, j, 1, arr[i][j]);
                 visited[i][j] = false;
             }
-
         }
     }
-
-    cout<< result <<'\n';
+    cout<< maxs;
     return 0;
+
 }
