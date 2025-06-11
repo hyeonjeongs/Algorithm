ddff636ca8440fbc829ca4d9abcc8957b4270f2a
hyeonjeongs <py0429@ewhain.net>
2025-06-11 22:18:50 +0900

    string

---
+++ string/9935.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str, pops, new_str="";
+    cin >> str;
+    cin >> pops;
+    int length= pops.size();
+    string res = "";
+
+    for(auto &ch: str) {
+        res+=ch;
+        if(res.size()>=length && res.substr(res.size()-length, length) == pops) {
+            res.erase(res.end()-length, res.end());
+        }
+    }
+
+
+    if(res==""){
+        res = "FRULA";
+    }
+    cout << res;
+    return 0;
+
+}
