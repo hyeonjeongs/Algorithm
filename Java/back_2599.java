6de811f27b9905e13904476122e9148ee59a6e90
hyeonjeongs <py0429@ewhain.net>
2025-08-16 23:45:32 +0900

    algorithm

--- Java/back_2599.java
+++ Java/back_2599.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class back_2599 {
-    public static void main(String[] args) throws IOException{
+     
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st= new StringTokenizer(br.readLine());
