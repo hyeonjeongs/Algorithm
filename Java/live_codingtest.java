ca6558bb2c86c1e6d7b5cf5ea545c0617e04a1d9
hyeonjeong <py0429@ewhain.net>
2026-02-03 14:14:11 +0900

    implementation

--- Java/live_codingtest.java
+++ Java/live_codingtest.java
@@ -1,11 +1,16 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class LiveCoding {
-    public void createStack() {
-
-    }
     public void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        List<Integer> list = new ArrayList<>();
+        int[] arr = list.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
 
     }
 }
